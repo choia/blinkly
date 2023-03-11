b201b764d97ff3ba5dfe0db94ee98c8e291df4a8
alex <alex.ji.choi@gmail.com>
2023-03-10 20:10:11 -0500

    refactor: apperror work in progress

--- blinkly/packages/blinkly-server/src/main.ts
+++ blinkly/packages/blinkly-server/src/main.ts
@@ -17,19 +17,26 @@ await server.register(fastifySwaggerUi, swaggerConfigUI)
 
 server.register(fastifyCookie)
 
-// server.setErrorHandler(async (error, request, reply) => {
-//   reply.statusCode = error.statusCode || 500
-//   if (error instanceof AppError) {
-//     reply.send({
-//       errorName: error.name,
-//       message: error.message,
-//       statusCode: error.statusCode,
-//       payload: error.payload,
-//     })
-//   }
-//   // console.log({ name: error.name })
-//   return error
-// })
+server.setErrorHandler(async (error, request, reply) => {
+  reply.statusCode = error.statusCode || 500
+  if (error instanceof AppError) {
+    return {
+      name: error.name,
+      message: error.message,
+      statusCode: error.statusCode,
+      payload: error.payload,
+    }
+  } else {
+    if (error.statusCode === 400) {
+      return {
+        name: 'BadRequest',
+        message: error.message,
+        statusCode: 400,
+      }
+    }
+  }
+  return error
+})
 
 server.register(authPlugin)
 server.register(routes)

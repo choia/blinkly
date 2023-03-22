e43aed04f892d7c6df1f23e293a3ee7a8551fdb1
alex <alex.ji.choi@gmail.com>
2023-03-22 07:48:41 -0400

    Add vector images

---
+++ blinkly/packages/blinkly-client/src/components/vectors/MoreVert.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+const SvgMoreVert = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M10.5 12a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM10.5 19a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM10.5 5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+export default SvgMoreVert

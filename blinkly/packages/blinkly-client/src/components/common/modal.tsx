f71fd7345372707f305139e2cb27dff4b1f142f8
alex <alex.ji.choi@gmail.com>
2023-04-12 20:09:48 -0400

    Implement modal

--- blinkly/packages/blinkly-client/src/components/common/modal.tsx
+++ blinkly/packages/blinkly-client/src/components/common/modal.tsx
@@ -11,7 +11,7 @@ interface Props {
 function Modal({ visible, className, children }: Props) {
   return (
     <>
-      <Overlay />
+      <Overlay visible={visible} />
       <PositionBox>
         <AnimatePresence>
           {visible && (
